2d20ad994dc888a8204da6c98ebe86e360e4ea6f
Jongha Park <jongha2788@u.sogang.ac.kr>
2023-11-25 06:28:17 +0900

    test: 여러 개의 쿠폰을 발급한다.

--- api/src/test/java/com/example/api/integration/ApplyServiceTest.java
+++ api/src/test/java/com/example/api/integration/ApplyServiceTest.java
@@ -1,19 +1,22 @@
 package com.example.api.integration;
 
+import static java.util.concurrent.Executors.newFixedThreadPool;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-import com.example.api.service.ApplyService;
 import com.example.api.repository.CouponRepository;
+import com.example.api.service.ApplyService;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
-@DataJpaTest
-@Import({ApplyService.class})
 @DisplayName("ApplyService 통합 테스트")
-public class ApplyServiceTest {
+@DataJpaTest
+@Import(ApplyService.class)
+class ApplyServiceTest {
 
     @Autowired
     private ApplyService applyService;
@@ -32,5 +35,32 @@ public class ApplyServiceTest {
 
         // then
         assertThat(count).isEqualTo(1L);
+    }
+
+    @Test
+    @DisplayName("여러개의 쿠폰을 발급한다")
+    void issueCoupons() throws InterruptedException {
+        // given
+        final int threadCount = 1000;
+        final int nThreads = 32;
+        final ExecutorService executorService = newFixedThreadPool(nThreads);
+        final CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+
+        // when
+        for (int i = 0; i < threadCount; i++) {
+            Long userId = (long) i;
+            executorService.submit(() -> {
+                try {
+                    applyService.applyCoupon(userId);
+                } finally {
+                    countDownLatch.countDown();
+                }
+            });
+        }
+        countDownLatch.await();
+
+        // then
+        final Long count = couponRepository.count();
+        assertThat(count).isEqualTo(100);
     }
 }
